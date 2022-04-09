4b930f719ed78f8a3aaaef06ac0d3dbc466aecd4
carlos <carlos.marca@meep.me>
2022-04-09 20:07:35 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "introduction-to-scala"
+
+version := "0.1"
+
+scalaVersion := "3.1.2"
---
+++ src/main/scala/inmutable.scala
@@ -0,0 +1,13 @@
+object inmutable {
+
+  // Define immutable variables for clubs 2♣ through 4♣
+  val twoClubs: Int = 2
+  val threeClubs: Int = 3
+  val fourClubs: Int = 4
+
+  def main(args: Array[String]): Unit = {
+    println(twoClubs)
+    println(threeClubs)
+    println(fourClubs)
+  }
+}
